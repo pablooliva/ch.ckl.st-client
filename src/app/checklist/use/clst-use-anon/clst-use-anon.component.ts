c8c366cd548d637a61926c15afb725a642e53519
Pablo Oliva <pablo@qecept.com>
2018-10-26 23:41:42 +0200

    Aesthetic improvements.

--- src/app/checklist/use/clst-use-anon/clst-use-anon.component.ts
+++ src/app/checklist/use/clst-use-anon/clst-use-anon.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { ClstBaseComponent } from "../../../shared/clst-base.component";
+import { RootListenerService } from "../../../shared/root-listener.service";
 
 @Component({
   selector: "clst-use-anon",
   templateUrl: "./clst-use-anon.component.html",
   styleUrls: ["./clst-use-anon.component.scss"]
 })
-export class ClstUseAnonComponent implements OnInit {
+export class ClstUseAnonComponent extends ClstBaseComponent implements OnInit {
   public link: string;
   public copyLabel: string;
 
-  constructor() {}
+  constructor(
+    private _router: Router,
+    private _el: ElementRef,
+    private _rootListener: RootListenerService
+  ) {
+    super(_router, _el, _rootListener);
+  }
 
   public ngOnInit(): void {
+    super.ngOnInit();
     this.copyLabel = "Copy";
     this.link = window.location.href;
   }
